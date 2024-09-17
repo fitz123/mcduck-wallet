eda1808b8d55ad1a3e32fd61356fc435764e3902
fitz123 <fitz123@users.noreply.github.com>
2024-09-16 22:40:41 -0300

    formatter

--- pkg/bot/helpers.go
+++ pkg/bot/helpers.go
@@ -1,7 +1,6 @@
 package bot
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/fitz123/mcduck-wallet/pkg/database"
@@ -9,25 +8,6 @@ import (
 )
 
 func formatTransactionHistory(transactions []database.Transaction) string {
-	if len(transactions) == 0 {
-		return messages.InfoNoTransactions
-	}
-
-	var response strings.Builder
-	response.WriteString(messages.InfoRecentTransactions)
-	for _, t := range transactions {
-		var description string
-		if t.Type == "transfer" {
-			if t.Amount < 0 {
-				description = fmt.Sprintf(messages.TransactionSent, -t.Amount, t.ToUsername)
-			} else {
-				description = fmt.Sprintf(messages.TransactionReceived, t.Amount, t.ToUsername)
-			}
-		} else {
-			description = fmt.Sprintf(messages.TransactionDeposited, t.Amount)
-		}
-		response.WriteString(fmt.Sprintf("%s - %s\n", t.Timestamp.Format("2006-01-02 15:04:05"), description))
-	}
-
-	return response.String()
+	formattedTransactions := messages.FormatTransactionHistory(transactions)
+	return messages.InfoRecentTransactions + strings.Join(formattedTransactions, "\n")
 }
