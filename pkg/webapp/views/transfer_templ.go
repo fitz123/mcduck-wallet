342e0ff8e11297c9f0e75f5cba087a374badd465
fitz123 <fitz123@users.noreply.github.com>
2024-09-25 00:04:34 -0300

    fixed back button

--- pkg/webapp/views/transfer_templ.go
+++ pkg/webapp/views/transfer_templ.go
@@ -36,11 +36,7 @@ func TransferForm(balances []database.Balance) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = showBackButton().Render(ctx, templ_7745c5c3_Buffer)
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<main><header><h2>Transfer Form</h2></header><form hx-post=\"/transfer\" hx-target=\"body\" hx-confirm=\"Are you sure you want to make this transfer?\"><label for=\"to_username\">Recipient Username <input type=\"text\" id=\"to_username\" name=\"to_username\" placeholder=\"@username\" required></label> <label for=\"amount\">Amount <input type=\"number\" id=\"amount\" name=\"amount\" min=\"1\" step=\"1\" required></label> <label for=\"currency\">Currency <select id=\"currency\" name=\"currency\" required>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<main data-page=\"transfer\"><header><h2>Transfer Form</h2></header><form hx-post=\"/transfer\" hx-target=\"body\" hx-confirm=\"Are you sure you want to make this transfer?\"><label for=\"to_username\">Recipient Username <input type=\"text\" id=\"to_username\" name=\"to_username\" placeholder=\"@username\" required></label> <label for=\"amount\">Amount <input type=\"number\" id=\"amount\" name=\"amount\" min=\"1\" step=\"1\" required></label> <label for=\"currency\">Currency <select id=\"currency\" name=\"currency\" required>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -52,7 +48,7 @@ func TransferForm(balances []database.Balance) templ.Component {
 			var templ_7745c5c3_Var2 string
 			templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(balance.Currency.Code)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `pkg/webapp/views/transfer.templ`, Line: 28, Col: 43}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `pkg/webapp/views/transfer.templ`, Line: 27, Col: 43}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 			if templ_7745c5c3_Err != nil {
@@ -65,7 +61,7 @@ func TransferForm(balances []database.Balance) templ.Component {
 			var templ_7745c5c3_Var3 string
 			templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(balance.Currency.Name)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `pkg/webapp/views/transfer.templ`, Line: 29, Col: 30}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `pkg/webapp/views/transfer.templ`, Line: 28, Col: 30}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 			if templ_7745c5c3_Err != nil {
@@ -78,7 +74,7 @@ func TransferForm(balances []database.Balance) templ.Component {
 			var templ_7745c5c3_Var4 string
 			templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(balance.Currency.Code)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `pkg/webapp/views/transfer.templ`, Line: 29, Col: 57}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `pkg/webapp/views/transfer.templ`, Line: 28, Col: 57}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 			if templ_7745c5c3_Err != nil {
@@ -91,7 +87,7 @@ func TransferForm(balances []database.Balance) templ.Component {
 			var templ_7745c5c3_Var5 string
 			templ_7745c5c3_Var5, templ_7745c5c3_Err = templ.JoinStringErrs(fmt.Sprintf("%.0f", balance.Amount))
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `pkg/webapp/views/transfer.templ`, Line: 29, Col: 109}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `pkg/webapp/views/transfer.templ`, Line: 28, Col: 109}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var5))
 			if templ_7745c5c3_Err != nil {
@@ -156,7 +152,7 @@ func TransferAlert(message string, isSuccess bool) templ.Component {
 		var templ_7745c5c3_Var9 string
 		templ_7745c5c3_Var9, templ_7745c5c3_Err = templ.JoinStringErrs(message)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `pkg/webapp/views/transfer.templ`, Line: 40, Col: 75}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `pkg/webapp/views/transfer.templ`, Line: 39, Col: 75}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var9))
 		if templ_7745c5c3_Err != nil {
